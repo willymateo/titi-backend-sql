af202e9514c5724b6f22330eb004f61bd17331c5
willymateo <wjmateo@espol.edu.ec>
2022-07-24 18:30:03 -0500

    configuration of db.config  for production, test and development environment

---
+++ src/config/db.config.mjs
@@ -0,0 +1,95 @@
+/* require("dotenv").config(); */
+
+/* module.exports = { */
+/*   development: { */
+/*     username: process.env.DEV_DB_USERNAME, */
+/*     password: process.env.DEV_DB_PASSWORD, */
+/*     database: process.env.DEV_DATABASE, */
+/*     host: process.env.DEV_DB_HOST, */
+/*     port: process.env.DEV_DB_PORT, */
+/*     dialect: "postgres", */
+/*     dialectOptions: { */
+/*       bigNumberStrings: true, */
+/*       supportBigNumbers: true, */
+/*     }, */
+/*     timezone: "-05:00", */
+/*   }, */
+/*   test: { */
+/*     username: process.env.TEST_DB_USERNAME, */
+/*     password: process.env.TEST_DB_PASSWORD, */
+/*     database: process.env.TEST_DATABASE, */
+/*     host: process.env.TEST_DB_HOST, */
+/*     port: process.env.TEST_DB_PORT, */
+/*     dialect: "postgres", */
+/*     dialectOptions: { */
+/*       bigNumberStrings: true, */
+/*       supportBigNumbers: true, */
+/*     }, */
+/*     timezone: "-05:00", */
+/*   }, */
+/*   production: { */
+/*     username: process.env.PROD_DB_USERNAME, */
+/*     password: process.env.PROD_DB_PASSWORD, */
+/*     database: process.env.PROD_DATABASE, */
+/*     host: process.env.PROD_DB_HOST, */
+/*     port: process.env.PROD_DB_PORT, */
+/*     dialect: "postgres", */
+/*     dialectOptions: { */
+/*       bigNumberStrings: true, */
+/*       supportBigNumbers: true, */
+/*       // ssl: { */
+/*       // ca: fs.readFileSync(__dirname + '/mysql-ca-main.crt') */
+/*       // } */
+/*     }, */
+/*     timezone: "-05:00", */
+/*   }, */
+/* }; */
+
+import "dotenv/config";
+
+const dbConfig = {
+  development: {
+    username: process.env.DEV_DB_USERNAME,
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.DEV_DATABASE,
+    host: process.env.DEV_DB_HOST,
+    port: process.env.DEV_DB_PORT,
+    dialect: "postgres",
+    dialectOptions: {
+      bigNumberStrings: true,
+      supportBigNumbers: true,
+    },
+    timezone: "-05:00",
+  },
+  test: {
+    username: process.env.TEST_DB_USERNAME,
+    password: process.env.TEST_DB_PASSWORD,
+    database: process.env.TEST_DATABASE,
+    host: process.env.TEST_DB_HOST,
+    port: process.env.TEST_DB_PORT,
+    dialect: "postgres",
+    dialectOptions: {
+      bigNumberStrings: true,
+      supportBigNumbers: true,
+    },
+    timezone: "-05:00",
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DATABASE,
+    host: process.env.PROD_DB_HOST,
+    port: process.env.PROD_DB_PORT,
+    dialect: "postgres",
+    dialectOptions: {
+      bigNumberStrings: true,
+      supportBigNumbers: true,
+      // ssl: {
+      // ca: fs.readFileSync(__dirname + '/mysql-ca-main.crt')
+      // }
+    },
+    timezone: "-05:00",
+  },
+};
+
+export default dbConfig;
