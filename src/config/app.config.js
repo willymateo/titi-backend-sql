eb6a148cab877d46e3d9cee959d63c4b3e89028e
willymateo <wjmateo@espol.edu.ec>
2022-07-24 13:54:13 -0500

    configuration to production

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 
-const saltRounds = 10;
+const saltRounds = parseInt(process.env.JWT_SALT_ROUNDS);
 const jwtSecret = process.env.JWT_SECRET;
 
 export { saltRounds, jwtSecret };
