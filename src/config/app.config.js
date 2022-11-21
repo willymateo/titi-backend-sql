cda37d77a5c770328b83a8e8e53036e132d812c7
willymateo <wjmateo@espol.edu.ec>
2022-11-20 22:41:31 -0500

    Add linebreak in logs

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 0;
 
 // morgan
 const morganFormat =
-  "● [:date[iso]] [:remote-addr :remote-user] :method :url HTTP/:http-version :status :response-time ms - :res[content-length]";
+  "\n● [:date[iso]] [:remote-addr :remote-user] :method :url HTTP/:http-version :status :response-time ms - :res[content-length]";
 
 // bcrypt
 const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS);
